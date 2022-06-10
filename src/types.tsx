9a9280e8fb53f24c727d5008a520b520eb5d8ad0
abdejohan <abdejohan@gmail.com>
2022-06-10 03:29:02 +0200

    Add type folder

---
+++ src/types.tsx
@@ -0,0 +1,17 @@
+type Question = {
+	key: number;
+	title: string;
+	description: string;
+	question: string | undefined;
+	choices: Array<string> | undefined;
+	inputs: Array<string> | undefined;
+	backButton: string | undefined;
+	nextButton: string | undefined;
+	buttonWidth: string | undefined;
+};
+
+type DataAttributeObject = {
+	questions: [Question];
+};
+
+export { Question, DataAttributeObject };
